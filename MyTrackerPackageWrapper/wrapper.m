c2a3d0146096b0c5fc759b9f3ea19429cdbd15c2
myTarget <51374782+mytarget-bot@users.noreply.github.com>
2021-07-29 19:30:51 +0300

    Revert last commit

--- MyTrackerPackageWrapper/wrapper.m
+++
@@ -1,9 +0,0 @@
-/*
- 
- THIS IS AN EMPTY FILE FOR CORRECT OPERATION OF SWIFT PACKAGE OF THE LIBRARY.
- 
- The software library myTracker (hereinafter referred to as the “Library”) is developed to be integrated
- (installed) in the mobile applications in order to collect data on its use (for example, installation data,
- application start-up rate, application use duration, etc.). The data acquired through the Library is
- available to the Licensor at the website https://tracker.my.com.
- */
